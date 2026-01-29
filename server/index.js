8af2c8998edac4d7186761df5114c3cabaefdef3
Himanshu Patel <himanshu123lmp@gmail.com>
2026-01-29 13:09:38 +0530

    Modified Cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}
+));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
